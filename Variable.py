4fdc0370f7cd2249c3136960b1367d370558099f
JOHN BUL <buljohnn@gmail.com>
2021-10-05 21:54:19 -0400

    variable assignment first commit

---
+++ Variable.py
@@ -0,0 +1,51 @@
+# VARIABLE ASSIGNMENT
+n = 300
+print(n)
+n = 1000
+print(n)
+
+# Chained Assignment
+a = b = c = 300
+print(a, b, c)
+
+# Variable Types in Python
+var = 23.5
+print(var)
+
+var = "Now I'm a string"
+print(var)
+
+# Object References
+print(300)
+type(300)
+
+n = 300
+print(n)
+type(n)
+
+m = n
+m = 400
+
+n = "Food"
+
+# Object Identity
+n = 300
+m = n
+id(n)
+id(m)
+
+m = 400
+id(m)
+
+# Variable Names
+name = "Bob"
+Age = 54
+has_W2 = True
+print(name, Age, has_W2)
+
+# Examples of Different variable names
+age = 1
+Age = 2
+AGe = 3
+AGE = 4
+print(age, Age, AGe, AGE)
